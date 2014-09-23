4071645a924cfa5590d2b249e2c8071698d982c8
Kieran92 <kboyle@ualberta.ca>
2014-09-23 13:54:14 -0600

    updating all files

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -6,11 +6,15 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -18,11 +22,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+//import com.example.MyFirstApp.DisplayMessageActivity;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class LonelyTwitterActivity extends Activity {
 
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	private static ArrayList<LonelyTweetModel> tweets;
+	private ArrayAdapter<LonelyTweetModel> adapter;
+	public int count;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -32,6 +43,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button countButton = (Button) findViewById(R.id.boot_counts);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -39,8 +51,11 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+				tweets.add(new LonelyTweetModel(text));
+				adapter.notifyDataSetChanged();
+				count++;
+				saveInFile();
+				
 
 			}
 		});
@@ -50,22 +65,24 @@ public class LonelyTwitterActivity extends Activity {
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		loadFromFile();
+		if (tweets ==null)
+			tweets = new ArrayList<LonelyTweetModel>();
+		
+		adapter = new ArrayAdapter<LonelyTweetModel>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+	private void loadFromFile() {
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+			Gson gson = new Gson();
+			//https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html sept 23 2014
+			Type listType = new TypeToken<ArrayList<LonelyTweetModel>>() {}.getType();
+			tweets = gson.fromJson(in, listType);
+
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -74,15 +91,15 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
 	}
 	
-	private void saveInFile(String text, Date date) {
+	private void saveInFile() {
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
+			FileOutputStream fos = openFileOutput(FILENAME,0);
+			Gson gson = new Gson();
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			gson.toJson(tweets,osw);
+			osw.flush();
 			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -92,4 +109,14 @@ public class LonelyTwitterActivity extends Activity {
 			e.printStackTrace();
 		}
 	}
+	
+	public static ArrayList<LonelyTweetModel> getNumber(){
+		return tweets;
+	}
+	
+	public void bootCounts(){
+		Intent intent = new Intent(this, Counts.class);
+		startActivity(intent);
+	}
+	
 }
